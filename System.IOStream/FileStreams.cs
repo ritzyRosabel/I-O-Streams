cd44575d451f1090869f9041b23bb93aecf0e817
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-03 16:39:44 +0100

    Filestream class

--- System.IOStream/FileStreams.cs
+++ System.IOStream/FileStreams.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace System.IOStream
 {
-    class Program
+    public class FileStreams
     {
-        static void Main(string[] args)
-        {
-
-        }
+      
     }
 }
