b20d10dc4a44e8309760fe3beffd342631f77e5f
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-03 23:34:25 +0100

    TextReader Class

---
+++ System.IOStream/TextReaders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IOStream
+{
+    public class TextReaders
+    {
+        public void ReadFile()
+        {
+            string path = ConfigurationManager.AppSettings["path"];
+            TextReader reader = File.OpenText(path+"\\r.txt");
+            Console.WriteLine("file open");
+            Console.WriteLine("reading file");
+            string data = reader.ReadToEnd();
+            Console.WriteLine(data);
+            reader.Close();
+        }
+    }
+}
