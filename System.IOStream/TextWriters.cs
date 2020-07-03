91e6d1a4c8b934ed36361aff4eb4f90705ee8914
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-03 23:15:42 +0100

    TextWriterClass

---
+++ System.IOStream/TextWriters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IOStream
+{
+    public class TextWriters
+    {
+        public void WriteFile()
+        {
+            string path = ConfigurationManager.AppSettings["path"];
+
+            TextWriter writer = File.CreateText(path);//Textwriter is an abstract class
+            writer.Write("wan ti mu hushpuppi, gucci gucci, ");
+            writer.WriteLine("yahoo boy don turn small puppy,");
+            writer.Write("wan ti mu Oga yin,you are still pressing");
+            writer.Write("ok i wont talk, i am watching");
+            writer.WriteLine(" i swear they will catch you");
+            writer.WriteLine("they will  catch you");
+
+        }
+    }
+}
