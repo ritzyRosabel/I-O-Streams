43064da74f48ca2a493cee31c1377aa81c17c24e
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-03 23:35:53 +0100

    execute

--- System.IOStream/Program.cs
+++ System.IOStream/Program.cs
@@ -38,12 +38,14 @@ namespace System.IOStream
 
             //stream reader closed
             //filestream closed
-            TextWriters text = new TextWriters();
-            text.WriteFile();
+            //TextWriters text = new TextWriters();
+            //text.WriteFile();
             //wan ti mu hushpuppi, gucci gucci, yahoo boy don turn small puppy,
             //wan ti mu Oga yin,you are still pressingok i wont talk, i am watching i swear they will catch you
             //they will  catch you
-            
+
+            TextReaders reader = new TextReaders();
+            reader.ReadFile();
             Console.ReadLine();
             // worked as soon as i removed all instance of filestream. apparently you cant have two streams accessing a file at the same time
             //F
